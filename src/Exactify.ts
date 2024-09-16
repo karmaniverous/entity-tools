6f4463c3dd2c07a716cfef0427778b85f1625391
Jason Williscroft <me@karmanivero.us>
2024-09-17 00:27:43 +0800

    improved exactify

--- src/Exactify.ts
+++ src/Exactify.ts
@@ -1,12 +1,18 @@
 /**
- * Strips the generic `[x: string]: unknown` property from `Record<string, unknown>` type.
+ * Strips the unspecified properties like `[x: string]: unknown` from `Record` types.
  *
- * @typeParam T - The `Record<string, unknown>` type.
+ * @typeParam T - The `Record` type.
  *
- * @returns The `Record<string, unknown>` type without the generic property.
+ * @returns The `Record` type with only specified properties.
  *
  * @category Utilities
  */
 export type Exactify<T extends Record<string, unknown>> = {
-  [P in keyof T as string extends P ? never : P]: T[P];
+  [P in keyof T as string extends P
+    ? never
+    : number extends P
+      ? never
+      : symbol extends P
+        ? never
+        : P]: T[P];
 };
