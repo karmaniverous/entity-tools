174b1a8b3da4772fd756766d38e78b8dedfc1282
Jason Williscroft <me@karmanivero.us>
2024-11-12 21:23:05 +0800

    updated docs

--- src/MakeOptional.ts
+++ src/MakeOptional.ts
@@ -1,8 +1,6 @@
 /**
  * Makes specified properties of T optional.
  *
- * If `T` has `string` index signature, properties will be `| undefined`.
- *
  * @typeParam T - The type to make properties optional.
  * @typeParam U - The properties to make optional.
  *
