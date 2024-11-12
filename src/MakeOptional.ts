edeccaec012414538a939c78563f0a7c1f87b35e
Jason Williscroft <me@karmanivero.us>
2024-11-12 21:17:46 +0800

    updated MakeOptional

--- src/MakeOptional.ts
+++ src/MakeOptional.ts
@@ -1,11 +1,13 @@
 /**
  * Makes specified properties of T optional.
  *
+ * If `T` has `string` index signature, properties will be `| undefined`.
+ *
  * @typeParam T - The type to make properties optional.
  * @typeParam U - The properties to make optional.
  *
  * @category Utilities
  */
 export type MakeOptional<T extends object, U extends keyof T> = {
-  [P in keyof T]: P extends U ? T[P] | undefined : T[P];
-};
+  [P in keyof T as P extends U ? never : P]: T[P];
+} & Partial<Pick<T, U>>;
