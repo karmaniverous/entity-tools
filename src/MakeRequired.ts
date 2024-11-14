f04fd6f8c816539967c3d9bbeb42fdc2c1378c0d
Jason Williscroft <me@karmanivero.us>
2024-11-14 15:23:38 +0800

    added MakeRequired

---
+++ src/MakeRequired.ts
@@ -0,0 +1,11 @@
+/**
+ * Makes specified properties of T required.
+ *
+ * @typeParam T - The type to make properties required.
+ * @typeParam U - The properties to make required.
+ *
+ * @category Utilities
+ */
+export type MakeRequired<T extends object, U extends keyof T> = {
+  [P in keyof T as P extends U ? never : P]: T[P];
+} & Required<Pick<T, U>>;
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ export type {
   FlattenEntityMap,
 } from './FlattenEntityMap';
 export type { MakeOptional } from './MakeOptional';
+export type { MakeRequired } from './MakeRequired';
 export type { AllDisjoint, MutuallyExclusive } from './MutuallyExclusive';
 export { isNil, type Nil } from './Nil';
 export type { NotNever } from './NotNever';
